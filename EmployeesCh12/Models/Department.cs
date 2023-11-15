88d9e0f250fefb9dabd429a2d2578535f719ddc1
simpyb761 <simpyb761@gmail.com>
2023-11-15 18:03:57 -0500

    Completed Program

---
+++ EmployeesCh12/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace EmployeesCh12.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Department")]
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<Employee> Employee { get; set; } = new HashSet<Employee>();
+
+        public ICollection<DepartmentLocation> DepartmentLocations { get; set; } = new HashSet<DepartmentLocation>();
+    }
+}
---
+++ EmployeesCh12/Models/DepartmentLocation.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        public int DepartmentID { get; set; }
+        public int LocationID { get; set; }
+
+        public Department Department { get; set; } = null!;
+        public Location Location { get; set; } = null!;
+    }
+}
